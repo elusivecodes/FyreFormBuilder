927f302298b2c9f0edfbd08b72fd425c9016d2a4
Elusive <elusivecodes@gmail.com>
2022-04-04 18:25:28 +1000

    Removed FyreError dependency

--- tests/InputTypeTest.php
+++ tests/InputTypeTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Tests;
 
 use
-    Fyre\Error\Exceptions\Exception,
+    BadMethodCallException,
     Fyre\FormBuilder\FormBuilder;
 
 trait InputTypeTest
@@ -31,7 +31,7 @@ trait InputTypeTest
 
     public function testInputTypeInvalid(): void
     {
-        $this->expectException(Exception::class);
+        $this->expectException(BadMethodCallException::class);
 
         FormBuilder::invalid();
     }
